16688e40a56b8c83c9ce489c7a00b49f075fe8a4
yusu Jang <dbtnss@naver.com>
2021-07-11 03:06:14 +0900

    Use alpha transparency

--- basic/index.js
+++ basic/index.js
@@ -1,6 +1,18 @@
 const loadCanvas = () => {
-  const canvas = document.querySelector('#canvas')
-  console.log(canvas)
+  const canvas = document.getElementById('canvas')
+  let ctx
+
+  if (canvas.getContext) {
+    ctx = canvas.getContext('2d')
+  } else {
+    return alert('캔버스가 존재하지 않습니다')
+  }
+
+  ctx.fillStyle = 'rgb(200, 0, 0)'
+  ctx.fillRect(10, 10, 50, 50)
+
+  ctx.fillStyle = 'rgba(0, 0, 200, 0.5)'
+  ctx.fillRect(30, 30, 50, 50)
 }
 
 window.addEventListener('load', () => {
